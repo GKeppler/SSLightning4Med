d136bdf4943c7a73523a1cd544ffef1db7fefc3f
GKeppler <gustavkeppler@gmail.com>
2022-05-06 12:36:54 +0200

    new implementaiton without double net

--- SSLightning4Med/models/train_mean_teacher.py
+++ SSLightning4Med/models/train_mean_teacher.py
@@ -12,6 +12,7 @@ from SSLightning4Med.models.train_CCT import consistency_loss
 from SSLightning4Med.utils.utils import sigmoid_rampup
 
 
+# old
 def update_ema_variables(model, ema_model, alpha, global_step):
     # Use the true average until the exponential average is more correct
     alpha = min(1 - 1 / (global_step + 1), alpha)
@@ -23,6 +24,9 @@ class MeanTeacherModule(BaseModule):
     def __init__(self, args: Any) -> None:
         super(MeanTeacherModule, self).__init__(args)
         self.consistency = 0.1
+        self.ema = EMA(self.net, 0.99)
+        self.ema.register()
+        # old
         self.net_ema = deepcopy(self.net)
         for param in self.net_ema.parameters():
             param.detach_()
@@ -36,15 +40,12 @@ class MeanTeacherModule(BaseModule):
         # unsupervised
         batch_unusupervised = batch["unlabeled"]
         unlabeled_image_batch, _, _ = batch_unusupervised
-        noise = torch.clamp(torch.randn_like(unlabeled_image_batch) * 0.1, -0.2, 0.2)
-        ema_inputs = unlabeled_image_batch + noise
+
+        self.ema.apply_shadow()
         with torch.no_grad():
-            ema_output = self.net_ema(ema_inputs)
-            # ema_output_soft = torch.softmax(ema_output, dim=1)
-
+            ema_output = self.net(unlabeled_image_batch)
+        self.ema.restore()
         outputs_unsup = self.net(unlabeled_image_batch)
-        # outputs_unsup_soft = torch.softmax(outputs_unsup, dim=1)
-
         consistency_weight = self.consistency * sigmoid_rampup(self.current_epoch)
         if self.global_step < 1000:
             unsupervised_loss = 0.0
@@ -57,7 +58,8 @@ class MeanTeacherModule(BaseModule):
 
     def optimizer_step(self, *args, **kwargs):
         super().optimizer_step(*args, **kwargs)
-        update_ema_variables(self.net, self.net_ema, 0.99, self.global_step)
+        self.ema.update()
+        # update_ema_variables(self.net, self.net_ema, 0.99, self.global_step)
 
     @staticmethod
     def pipeline(dataModule: SemiDataModule, trainer: pl.Trainer, checkpoint_callback: ModelCheckpoint, args) -> None:
@@ -65,3 +67,45 @@ class MeanTeacherModule(BaseModule):
         dataModule.mode = "semi_train"
         trainer.fit(model=model, datamodule=dataModule)
         trainer.test(datamodule=dataModule, ckpt_path=checkpoint_callback.best_model_path)
+
+
+class EMA:
+    """
+    Implementation from https://fyubang.com/2019/06/01/ema/
+    """
+
+    def __init__(self, model, decay):
+        self.model = model
+        self.decay = decay
+        self.shadow = {}
+        self.backup = {}
+
+    def load(self, ema_model):
+        for name, param in ema_model.named_parameters():
+            self.shadow[name] = param.data.clone()
+
+    def register(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                self.shadow[name] = param.data.clone()
+
+    def update(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                new_average = (1.0 - self.decay) * param.data + self.decay * self.shadow[name]
+                self.shadow[name] = new_average.clone()
+
+    def apply_shadow(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                self.backup[name] = param.data
+                param.data = self.shadow[name]
+
+    def restore(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.backup
+                param.data = self.backup[name]
+        self.backup = {}
